0b9e09e8a941e3b6c20f22f7601701d4b9d87af7
spkal01 <kalligeross@gmail.com>
2023-12-01 12:59:00 +0100

    Blackiron: CLock Customization

--- src/com/blackiron/settings/fragments/StatusBarSettings.java
+++ src/com/blackiron/settings/fragments/StatusBarSettings.java
@@ -19,12 +19,20 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
-
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
-
+import org.blackiron.support.preferences.SystemSettingSwitchPreference;
+import org.blackiron.support.preferences.SystemSettingSeekBarPreference;
+import org.blackiron.support.preferences.SecureSettingSwitchPreference;
+import org.blackiron.support.preferences.SystemSettingListPreference;
 import com.android.settings.SettingsPreferenceFragment;
+import org.blackiron.support.preferences.SystemSettingSeekBarPreference;
 import com.android.settings.Utils;
 import android.util.Log;
 
@@ -37,6 +45,10 @@ import java.util.Collections;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+
+    private SystemSettingListPreference mStatusBarClock;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -44,12 +56,21 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.blackiron_settings_statusbar);
 
         PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
 
+        mStatusBarClock =
+                (SystemSettingListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
+
+        // Adjust status bar preferences for RTL
+        if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_rtl);
+            mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_rtl);
+        }
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         return false;
     }
 
